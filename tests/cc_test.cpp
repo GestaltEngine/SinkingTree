58530847310bdd904369bf50b74b350bbfb2e5fe
Sabdenov Chingiz <sabdenovchingiz@yandex.ru>
2023-03-09 23:52:22 +0300

    simple stress test

---
+++ tests/cc_test.cpp
@@ -0,0 +1,29 @@
+#include "unordered_cc_map.h"
+#include "runner.h"
+#include "commons.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <ranges>
+
+TEST_CASE("Multistress") {
+    HashTree<int, int> my(16);
+    const auto kNumThreads = GENERATE(2u, 4u, 8u);
+
+    const int kNumIterations = 1'000'000;
+
+    Runner runner{kNumIterations};
+    for (auto i : std::views::iota(0u, kNumThreads)) {
+        Random rand{i};
+        runner.Do([&my, rand]() mutable {
+            auto choice = rand() % 100;
+            if (choice < 20) {
+                my.Put(rand(), 1);
+            } else if (choice < 40) {
+                my.Erase(rand());
+            } else {
+                my.Get(rand());
+            }
+        });
+    }
+}
